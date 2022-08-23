d574259cc7d2d4b06bc7155b86d1e01dbceabd5e
Skylar Mcdermott <sky@Skylars-MacBook-Air-2.local>
2022-08-23 00:21:25 -0400

    implemented localhost only api acess and update argument

--- backend/soiledapi.py
+++ backend/soiledapi.py
@@ -1,15 +1,38 @@
+# soiled
+# Created by Skylar McDermott
+#############################
+
 from flask import Flask, abort, request
 from flask_restful import Api, Resource, reqparse
 import json
 # determines soil temp, current weather, future percipitation, and then returns it
 import get_data
+
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument("update", type=bool)
 
-# get data will create dict
+# api should only be accessible to localhost
+@app.before_request
+def block():
+    if request.remote_addr != "127.0.0.1":
+        abort(403)
+
+
 class soiledApi(Resource):
     def get(self):
-        return get_data.parse().json()
+        args = parser.parse_args()
+        if args['update'] == True:
+            return get_data.reader().parse(up=True)
+        else:
+            return get_data.reader().parse()
+
     
+api.add_resource(soiledApi, "/api")
 if __name__ == "__main__":
-    api.add_resource(soiledApi)
+    pass
+    app.run(debug=True)
+
+
+# skylar, implement a get 
